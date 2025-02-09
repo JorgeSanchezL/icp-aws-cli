7c5be48cba158bff506342f15896e48d788e86e5
Jorge Sanchez Lorente <jorgesanlor1221@gmail.com>
2025-02-09 18:21:38 +0100

    Add a limit to the cloudwatch requests

--- cmd/icp-aws-cli/cloudwatch/commands/logs/events/get.go
+++ cmd/icp-aws-cli/cloudwatch/commands/logs/events/get.go
@@ -13,27 +13,30 @@ import (
 func InitGetLogEventsCommand(cwLogsClient *cloudwatchlogs.Client, cloudWatchCmd *cobra.Command) {
 	var logGroupName string
 	var logStreamName string
+	var limit int32
 
 	var getLogEventsCmd = &cobra.Command{
 		Use:   "get-log-events",
-		Short: "Gets log events from a CloudWatch log stream",
+		Short: "Gets the last log events from a CloudWatch log stream (10 by default)",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if logGroupName == "" || logStreamName == "" {
 				return fmt.Errorf("log group name and log stream name must be specified")
 			}
-			return getLogEvents(cwLogsClient, logGroupName, logStreamName)
+			return getLogEvents(cwLogsClient, logGroupName, logStreamName, limit)
 		},
 	}
 
 	getLogEventsCmd.Flags().StringVarP(&logGroupName, "log-group-name", "g", "", "Name of the log group")
 	getLogEventsCmd.Flags().StringVarP(&logStreamName, "log-stream-name", "s", "", "Name of the log stream")
+	getLogEventsCmd.Flags().Int32VarP(&limit, "limit", "l", 10, "Number of log events to get")
 	cloudWatchCmd.AddCommand(getLogEventsCmd)
 }
 
-func getLogEvents(cwLogsClient *cloudwatchlogs.Client, logGroupName, logStreamName string) error {
+func getLogEvents(cwLogsClient *cloudwatchlogs.Client, logGroupName, logStreamName string, limit int32) error {
 	result, err := cwLogsClient.GetLogEvents(context.TODO(), &cloudwatchlogs.GetLogEventsInput{
 		LogGroupName:  &logGroupName,
 		LogStreamName: &logStreamName,
+		Limit:         &limit,
 	})
 	if err != nil {
 		return fmt.Errorf("could not get log events: %w", err)
--- cmd/icp-aws-cli/cloudwatch/commands/logs/streams/list.go
+++ cmd/icp-aws-cli/cloudwatch/commands/logs/streams/list.go
@@ -12,25 +12,28 @@ import (
 
 func InitListLogStreamsCommand(cwLogsClient *cloudwatchlogs.Client, cloudWatchCmd *cobra.Command) {
 	var logGroupName string
+	var limit int32
 
 	var listLogStreamsCmd = &cobra.Command{
 		Use:   "list-log-streams",
-		Short: "Lists CloudWatch log streams in a log group",
+		Short: "Lists the last CloudWatch log streams in a log group (10 by default)",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if logGroupName == "" {
 				return fmt.Errorf("log group name must be specified")
 			}
-			return listLogStreams(cwLogsClient, logGroupName)
+			return listLogStreams(cwLogsClient, logGroupName, limit)
 		},
 	}
 
 	listLogStreamsCmd.Flags().StringVarP(&logGroupName, "log-group-name", "n", "", "Name of the log group")
+	listLogStreamsCmd.Flags().Int32VarP(&limit, "limit", "l", 10, "Number of log streams to list")
 	cloudWatchCmd.AddCommand(listLogStreamsCmd)
 }
 
-func listLogStreams(cwLogsClient *cloudwatchlogs.Client, logGroupName string) error {
+func listLogStreams(cwLogsClient *cloudwatchlogs.Client, logGroupName string, limit int32) error {
 	result, err := cwLogsClient.DescribeLogStreams(context.TODO(), &cloudwatchlogs.DescribeLogStreamsInput{
 		LogGroupName: &logGroupName,
+		Limit:        &limit,
 	})
 	if err != nil {
 		return fmt.Errorf("could not list log streams: %w", err)
